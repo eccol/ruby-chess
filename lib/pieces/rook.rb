afbe75a8fd7421734d724e3ab7b371ef56b151a8
Eric Olsen <126840543+eccol@users.noreply.github.com>
2023-04-14 18:57:14 -0400

    Allow rook capturing opponent

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -4,28 +4,28 @@ class Rook < Piece
     # All spaces up down left and right until blocked
     (col + 1..8).each do |x|
       unless board.piece_at([x, row]).nil?
-        destinations.push([x, row])
+        destinations.push([x, row]) if board.piece_at([x, row]).color != color
         break
       end
       destinations.push([x, row])
     end
     (1..col - 1).each do |x|
       unless board.piece_at([x, row]).nil?
-        destinations.push([x, row])
+        destinations.push([x, row]) if board.piece_at([x, row]).color != color
         break
       end
       destinations.push([x, row])
     end
     (row + 1..8).each do |y|
       unless board.piece_at([col, y]).nil?
-        destinations.push([col, y])
+        destinations.push([col, y]) if board.piece_at([col, y]).color != color
         break
       end
       destinations.push([col, y])
     end
     (1..row - 1).each do |y|
       unless board.piece_at([col, y]).nil?
-        destinations.push([col, y])
+        destinations.push([col, y]) if board.piece_at([col, y]).color != color
         break
       end
       destinations.push([col, y])
