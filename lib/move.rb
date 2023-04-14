a154fdfa9c3177cd4a2ec1f45a90cb6e551d7f2d
Eric Olsen <126840543+eccol@users.noreply.github.com>
2023-04-14 18:24:01 -0400

    Initialize piece.rb

--- lib/move.rb
+++ lib/move.rb
@@ -10,8 +10,11 @@ class Move
 
   def valid?(board)
     # Get the piece at current location, if none then false
+    piece = board.piece_at(origin)
     # Ask the piece where it can move given board
+    possible_destinations = piece.move_range
     # If destination in acceptable areas, true
+    possible_destinations.include? destination
   end
 
   def command?
