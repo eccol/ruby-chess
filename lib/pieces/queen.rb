453c40f47bc4554870e9cefc3ecc058c955c37a6
Eric Olsen <126840543+eccol@users.noreply.github.com>
2023-04-14 21:49:49 -0400

    Queen movement

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -1,6 +1,13 @@
 class Queen < Piece
+  include StraightMovement
+  include DiagonalMovement
+
   def to_s
     return '♕' if @color == :black
     return '♛' if @color == :white
+  end
+
+  def move_range
+    check_left + check_right + check_up + check_down + check_upleft + check_upright + check_downleft + check_downright
   end
 end
