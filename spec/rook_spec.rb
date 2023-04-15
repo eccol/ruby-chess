ccc84f235f1d8c9369421c014011071add274b18
Eric Olsen <126840543+eccol@users.noreply.github.com>
2023-04-14 20:09:18 -0400

    Initial Rook movement tests

---
+++ spec/rook_spec.rb
@@ -0,0 +1,38 @@
+require_relative 'spec_helper'
+require_relative '../lib/board'
+require_relative '../lib/piece'
+require_relative '../lib/pieces/rook'
+
+describe Rook do
+  describe '.move_range' do
+    let(:board) { Board.new({}) }
+    context 'when board is empty' do
+      it 'can move from bottom to top' do
+        board.board[[1, 1]] = Rook.new(board, :white, 1, 1)
+        rook = board.piece_at([1, 1])
+        expect(rook.move_range.include?([1, 8])).to eq(true)
+      end
+      it 'can move from left to right' do
+        board.board[[1, 1]] = Rook.new(board, :white, 1, 1)
+        rook = board.piece_at([1, 1])
+        expect(rook.move_range.include?([8, 1])).to eq(true)
+      end
+    end
+    context 'when an enemy piece is on the board' do
+      it 'can capture' do
+        board.board[[1, 1]] = Rook.new(board, :white, 1, 1)
+        rook = board.piece_at([1, 1])
+        board.board[[1, 8]] = Rook.new(board, :black, 1, 8)
+        expect(rook.move_range.include?([1, 8])).to eq(true)
+      end
+    end
+    context 'when an ally piece is on the board' do
+      it 'cannot capture' do
+        board.board[[1, 1]] = Rook.new(board, :white, 1, 1)
+        rook = board.piece_at([1, 1])
+        board.board[[1, 8]] = Rook.new(board, :white, 1, 8)
+        expect(rook.move_range.include?([1, 8])).to eq(false)
+      end
+    end
+  end
+end
