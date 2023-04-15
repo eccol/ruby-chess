1fe0d48e2d0c890c59452a5d3c87baf66e7ea2a9
Eric Olsen <126840543+eccol@users.noreply.github.com>
2023-04-14 22:06:10 -0400

    Knight movement

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -3,4 +3,17 @@ class Knight < Piece
     return '♘' if @color == :black
     return '♞' if @color == :white
   end
+
+  def move_range
+    destinations = []
+    possibilities = [[col + 1, row + 2], [col - 1, row + 2], [col + 1, row - 2], [col - 1, row - 2], [col + 2, row + 1], [col + 2, row - 1],
+                     [col - 2, row + 1], [col - 2, row - 1]]
+    possibilities.each do |sq|
+      if board.piece_at(sq).nil?
+        destinations.push(sq)
+      elsif board.piece_at(sq).color != color
+        destinations.push(sq)
+      end
+    end
+  end
 end
